2ac4ea41261ac7ad1518e85bf458ea50e2a5bd60
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-06-21 09:59:46 +0100

    feature: client and server chat app source code

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ A simple Python Client-Server UDP Chat App for learning Networking Fundamentals.
 To run this UDP chat app on your local machine, you would open two terminal windows.
 
 - In one, you would start the server by running `python3 udp.py server -p 3000`.
-- In the other, you would start the client by running `python3 your_script.py client -p 3000`.
+- In the other, you would start the client by running `python3 udp.py client -p 3000`.
 
 The -p 3000 is optional since the code defaults to port 3000 if no port is specified.
--- udp.py
+++ udp.py
@@ -1,14 +1,44 @@
+import argparse
 import socket
 
 MAX_SIZE_BYTES = 65535  # Mazimum size of a UDP datagram
 
 
 def server(port):
-    host = "127.0.0.1"
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect((host, port))
+    host = "127.0.0.1"
+    s.bind((host, port))
+    print("Listening at {}".format(s.getsockname()))
+    while True:
+        data, clientAddress = s.recvfrom(MAX_SIZE_BYTES)
+        message = data.decode("ascii")
+        print("The client at {} says {!r}".format(clientAddress, message))
+        response_message = input("Response: ")
+        data = response_message.encode("ascii")
+        s.sendto(data, clientAddress)
 
 
 def client(port):
-    pass
-    # Your code goes here
+    host = "127.0.0.1"
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    while True:
+        message = input("Message: ")
+        data = message.encode("ascii")
+        s.send(data)
+        print("The OS assigned the address {} to me".format(s.getsockname()))
+        data = s.recv(MAX_SIZE_BYTES)
+        text = data.decode("ascii")
+        print("The server replied with {!r}".format(text))
+
+
+if __name__ == "__main__":
+    funcs = {"client": client, "server": server}
+    parser = argparse.ArgumentParser(description="UDP client and server")
+    parser.add_argument("functions", choices=funcs, help="client or server")
+    parser.add_argument(
+        "-p", metavar="PORT", type=int, default=3000, help="UDP port (default 3000)"
+    )
+    args = parser.parse_args()
+    function = funcs[args.functions]
+    function(args.p)
